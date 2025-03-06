2542347de4410a0603dad843fdacc41f358dd145
Ed <edward.hartnett@noaa.gov>
2025-03-06 12:20:47 -0700

    Now using wikipedia tool

--- MAbot/src/MAbot.py
+++ MAbot/src/MAbot.py
@@ -1,5 +1,7 @@
 from langchain_community.tools import WikipediaQueryRun 
 from langchain_community.utilities import WikipediaAPIWrapper
+import streamlit as st
+import pandas as pd
 
 class MAbot:
     def __init__(self):
@@ -7,6 +9,12 @@ class MAbot:
 
     def main(self):
         print("MAbot main function")
+        api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
+        tool = WikipediaQueryRun(api_wrapper=api_wrapper)
+        results = tool.run("Machine Learning")
+        print(results)
+        st.write(results)
+        
 
 if __name__ == "__main__":
     my = MAbot()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MAbot
 A chatbot that knows about martial arts.
+
+Based on [Generative AI with LangChain](https://github.com/benman1/generative_ai_with_langchain)
