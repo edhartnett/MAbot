5cb93f16beefefa0d160d0ba01c547871f48fbeb
Ed <edward.hartnett@noaa.gov>
2025-03-06 11:32:26 -0700

    startup

---
+++ MAbot/src/MAbot.py
@@ -0,0 +1,13 @@
+from langchain_community.tools import WikipediaQueryRun 
+from langchain_community.utilities import WikipediaAPIWrapper
+
+class MAbot:
+    def __init__(self):
+        print("MAbot initialized")
+
+    def main(self):
+        print("MAbot main function")
+
+if __name__ == "__main__":
+    my = MAbot()
+    my.main()
